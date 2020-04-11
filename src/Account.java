054a22f696339351cbcceefc94546f2c414d077c
Sklyr <Sklyr@bk.ru>
2020-04-11 17:47:04 +1000

    Initial

---
+++ src/Account.java
@@ -0,0 +1,8 @@
+public class Account {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int payment = 500_000_000;
+        int total = account + payment;
+        System.out.println (total);
+    }
+}
